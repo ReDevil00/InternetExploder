db1c037be55c8cd2953032bd244c47a475cf9dc8
Brenna Lusk <blusk218@gmail.com>
2021-03-27 22:08:01 -0500

    Bug, Corrupt, Malware, and Virus now have collitions
    Touching one sends you back to the intro scene

--- Assets/HandleCollisions.cs
+++ Assets/HandleCollisions.cs
@@ -7,7 +7,6 @@ using UnityEngine.SceneManagement;
 public class HandleCollisions : MonoBehaviour
 {
     private Rigidbody2D rb2D;
-    public GameObject cursor;
 
     // Start is called before the first frame update
     void Start()
