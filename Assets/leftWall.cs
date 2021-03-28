5845d8c2c93cceefc2ec813ff5c787c614135540
ReDevil00 <hannahparks40@gmail.com>
2021-03-27 20:49:15 -0500

    left wall

---
+++ Assets/leftWall.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leftWall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
