17cef70808894e8fcdad2fcbadde4138c24fa505
Brenna Lusk <blusk218@gmail.com>
2021-03-27 21:49:51 -0500

    Bug has collisions now!

---
+++ Assets/HandleCollisions.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HandleCollisions : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+    public GameObject cursor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        //THIS WORKS :D
+        
+    }
+}
