58a2519cd19811e9d0ded35abe0eab85b21fc43a
ReDevil00 <hannahparks40@gmail.com>
2021-03-28 02:38:42 -0500

    audio

--- Assets/exploder.cs
+++ Assets/exploder.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class exploder : MonoBehaviour
 {
+    public AudioClip click;
     public GameObject cursor;
     public GameObject recycle;
     public GameObject greyExploder;
@@ -12,17 +13,20 @@ public class exploder : MonoBehaviour
     void Start()
     {
         greyExploder = GetComponent<GameObject>();
+        GetComponent<AudioSource>().playOnAwake = false;
+        GetComponent<AudioSource>().clip = click;
     }
 
     void OnTriggerStay2D(Collider2D collider)
     {
         if(collider.gameObject.name == "cursor")
         {
+            
             GameObject.Find("cursor").GetComponent<InputScreenPlayer>().hasExploder = true;
             Instantiate(recycle, new Vector3(7.75f, 3.0f), Quaternion.identity);
             GameObject.Find("exploderGray").transform.parent = cursor.transform;
+            GetComponent<AudioSource>().Play();
 
-            //make click sound
         }
     }
 }
